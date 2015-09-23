5979456e2fa50eeae39df569f40a3972c0b24da5
Jordan Kicklighter <jwkicklighter@yahoo.com>
2015-09-22 21:15:55 -0400

    Added shortcodes file.

--- functions.php
+++ functions.php
@@ -305,6 +305,11 @@ require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/custom-header.php';
 
 /**
+ * Custom Shortcodes.
+ */
+require get_template_directory() . '/inc/shortcodes.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
